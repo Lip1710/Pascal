9dbf67fde775f8459c5ccddcf89415c3f42a27f3
Lip1710 <Lip1710@mail.ru>
2018-01-21 13:58:44 +0300

    Improve formatting, change writeln

--- 1_course/recurs.pas
+++ 1_course/recurs.pas
@@ -1,26 +1,23 @@
 program sumNumbers1;
 var n,sum: integer;
 procedure sumNumbers(n: integer);
-begin
-
- if n>0 then
  begin
-  sum:=sum+n;
-  n:= n-1;
-//  writeln(n);
-
- sumNumbers(n) ;
- end
- else
- writeln('end');
-
+  if n>0 then
+  begin
+   sum:=sum+n;
+   n:= n-1;
+   //  writeln(n);
+   sumNumbers(n) ;
+  end
+  else
+  //writeln('end');
  end;
-
+// MAIN CODE
 begin
-sum:=0;
+ sum:=0;
+ write('Enter numbers for find sum numbers from 1 to this: ');
  readln(n);
- writeln(' recursive sumNumbers');
-  sumNumbers(n);
-  writeln(sum);
-  readln();
+ sumNumbers(n);
+ writeln('Sum numbers from 1 to ',n,' = ', sum);
+ //readln();
 end.
