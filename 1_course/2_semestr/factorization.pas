d179af6fb996012883584a59b2508f36d4939086
Lip1710 <Lip1710@mail.ru>
2018-02-12 02:38:02 +0300

    add programm factorization

--- 1_course/2_semestr/factorization.pas
+++ 1_course/2_semestr/factorization.pas
@@ -1,15 +1,21 @@
 //Lotareva Irina.
-program factorization;
- var n,f:integer;
-function fact(n: Integer):Integer;
+program factorize;
+var  x,i: integer;
+ procedure fact(x:integer);
  begin
-  if n>0 then Result:= n + fact(n-1) else Result:=0;
+  for i:=2 to x do begin
+   if (x mod i)=0 then begin
+     x:=(x div i);
+     write(' ',i);
+     fact(x);
+   end;
+  end;
  end;
 
-begin
-  write(' Enter a digit for factorization: ');
-  readln(n);
-  f:=fact(n);
-  writeln(' Recursive factorization of ',n,' is ',f);
-  readln();
-end.
+ Begin
+  write('Enter the number for factorization : ');
+  readln(x);
+  writeln(' Recursive factorization of ',x,' is :');
+  fact(x);
+  writeln();
+ End.
