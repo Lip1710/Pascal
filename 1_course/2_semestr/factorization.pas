d0f8dfdbf0868f1c0af596bfa8a6539868c7ed79
Lip1710 <Lip1710@mail.ru>
2018-02-15 23:58:55 +0300

    Add asterics

--- 1_course/2_semestr/factorization.pas
+++ 1_course/2_semestr/factorization.pas
@@ -1,12 +1,15 @@
 //Lotareva Irina. Homework 1
 program factorize;
+//uses CRT;
 var  x,i: integer;
+     a: string;
  procedure fact(x:integer);
  begin
   for i:=2 to x do begin
    if (x mod i)=0 then begin
      x:=(x div i);
-     write(' ',i);
+     write(a,i);
+     a:='*';
      fact(x);
    end;
   end;
@@ -16,6 +19,7 @@ var  x,i: integer;
   write('Enter the number for factorization : ');
   readln(x);
   writeln(' Recursive factorization of ',x,' is :');
+  a:=' ';
   fact(x);
   writeln();
  End.
