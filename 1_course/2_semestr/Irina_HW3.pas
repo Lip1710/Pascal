d59bada856f4f6f0db49c55569c1b96907d753f7
Lip1710 <Lip1710@mail.ru>
2018-02-24 11:22:16 +0300

    Add Homwwork 3 (sorting 2-dim array)

---
+++ 1_course/2_semestr/Irina_HW3.pas
@@ -0,0 +1,42 @@
+// Irina Homework 3. Sort two-Dimensional array, even row - ascending; odd - descending
+program SortArray;
+const row=10;
+      col=5;
+      maxn=10;
+var  m: array [1..row,1..col] of integer;
+     c,i,j,b: integer;
+
+procedure PrintArray ;
+  begin
+    for c:=1 to col do begin
+       write(c:2,': ');
+       for i:=1 to row do
+           write(m[i,c]:3);
+       writeln;
+    end;
+    writeln;
+  end;
+
+begin
+  cls;
+  // fill in array with random numbers
+  for c:=1 to col do
+     for i:=1 to row do
+        m[i,c]:=random(maxn);
+
+  writeln('Random array:');
+  PrintArray;
+
+  // sort array: even row - ascending; odd - descending
+  for c:=1 to col do
+    for j:=1 to row-1 do
+      for i:=1 to row-1 do
+       if ((m[i,c])>(m[i+1,c])) xor (odd(c)) then begin
+         b:=m[i,c];
+         m[i,c]:=m[i+1,c];
+         m[i+1,c]:=b;
+       end;
+
+  writeln('Sorted array:');
+  PrintArray;
+end.
