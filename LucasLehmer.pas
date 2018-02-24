9a54cb9b17089296d8b51585ed79358f59e5ae67
Lip1710 <Lip1710@mail.ru>
2018-02-24 11:25:04 +0300

    Tesiting LucasLehmer algoritm

--- LucasLehmer.pas
+++ LucasLehmer.pas
@@ -7,10 +7,7 @@ begin
   n := 1;
   for exponent := 2 to 31 do
   begin
-    if exponent = 2 then
-      s := 0
-    else
-      s := 4;
+    if exponent = 2 then  s := 0  else  s := 4;
     n := (n + 1)*2 - 1;  // This saves from needing the math unit for exponentiation
     writeln('n = ', n);
     for i := 1 to exponent-2 do
