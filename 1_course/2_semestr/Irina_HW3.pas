7d804035fc4ceb48739673c99606ef96f911b47f
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-02-25 22:48:31 +0300

    optimised

--- 1_course/2_semestr/Irina_HW3.pas
+++ 1_course/2_semestr/Irina_HW3.pas
@@ -1,4 +1,4 @@
-// Irina Homework 3. Sort two-Dimensional array, even row - ascending; odd - descending
+// Irina Homework 3. Sort two-Dimensional array, even line - ascending; odd - descending
 program SortArray;
 const line=10;
       col=5;
@@ -30,7 +30,7 @@ begin
   // sort array: even line - ascending; odd - descending
   for c:=1 to col do
     for j:=1 to line-1 do
-      for i:=1 to line-1 do
+      for i:=1 to line-j do
        if ((m[i,c])>(m[i+1,c])) xor (odd(c)) then begin
          b:=m[i,c];
          m[i,c]:=m[i+1,c];
