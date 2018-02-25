b994ef49ad4ca10860520c54184029e9d4d67b8f
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-02-25 22:41:14 +0300

    optimize bubble sort

--- 1_course/2_semestr/bubble sort.pas
+++ 1_course/2_semestr/bubble sort.pas
@@ -4,36 +4,27 @@ var i,j,c,b:integer;m:array[0..n]of integer;
 begin
  cls;
  writeln('Enter from 2 to 15 integer numbers : ');
-  i:=0;
-  for i:=0 to n do
+ i:=0;
+ for i:=0 to n do
+    readln(m[i]);
+ writeln('Unsorted : ');
+ for i:=0 to n do
+    write(m[i]:3);
+ writeln;
 
-   readln(m[i]);
-   writeln('Unsorted : ');
-   for i:=0 to n do
-   write(m[i]:3);
-   writeln;
-   writeln('Now the sorted');
+ for j:=1 to n-1 do
+  for i:=0 to n-j do
+   if (m[i])>(m[i+1]) then
+    begin
+     b:=m[i];
+     m[i]:=m[i+1];
+     m[i+1]:=b;
+    end;
 
-  for j:=1 to n-1 do
-  for i:=0 to n-1 do
-  if (m[i])>(m[i+1]) then
-   begin
-   b:=m[i];
-   m[i]:=m[i+1];
-   m[i+1]:=b;
-   end;
-
-
-
-
-
+ writeln('Now the sorted');
  for i:=0 to n do
    write (m[i] :3 );
-   
-   //writeln(m[i+1],m[i])
-   //else
-    //writeln(m[i],m[i+1]);
-   
+    
  end.
    
    
