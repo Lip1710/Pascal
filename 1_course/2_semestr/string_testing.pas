28946de9a719ff41c0797fc25f7de3236cf5b893
Lip1710 <Lip1710@mail.ru>
2018-03-07 19:41:28 +0300

    Homework from 05.03.18

---
+++ 1_course/2_semestr/string_testing.pas
@@ -0,0 +1,52 @@
+program letters;
+//uses CRT;
+procedure FindMinMax (s: string);
+ var     i,min,max,n: integer;
+ begin
+   //ClrScr;
+   n:=0;
+   min:=length(s);
+   max:=0;
+    for i:= 1 to length(s)do
+     begin
+       if s[i]<> ' ' then begin
+          n:=n+1;
+          if n>max then max:=n;
+          if (i=length(s)) and (n<min) then min:=n;
+          end
+       else begin
+          if (n>0) and (n<min) then min:=n;
+          n:=0;
+       end;
+     end;
+     writeln(s,'  Long=',max,' Short=',min);
+ end;
+ 
+begin
+ cls;
+ FindMinMax('1');
+ FindMinMax(' 1');
+ FindMinMax(' 1 ');
+ FindMinMax('1 22');
+ FindMinMax(' 1 22 ');
+ FindMinMax('1 22 333');
+ FindMinMax(' 1 22 333 ');
+ FindMinMax('1 22 333 444');
+ FindMinMax(' 1 22 333 444 ');
+ FindMinMax('11 2 333');
+ FindMinMax(' 11 2 333 ');
+ FindMinMax('1111 222 33');
+ FindMinMax(' 1111 222 33 ');
+ FindMinMax('1111 222 3 ');
+ FindMinMax(' 1111 222 3');
+ FindMinMax(' 1111 22222 33');
+ FindMinMax(' 1111 22222 33 ');
+ FindMinMax(' 1111');
+ FindMinMax(' 1111 ');
+ FindMinMax(' 11111 2222 444');
+ FindMinMax(' 111111 222 3333333 55');
+ FindMinMax(' мама мыла раму рамище');
+ FindMinMax('  мама мыла юлу рамище');
+ 
+
+end.
--- 1_course/2_semestr/новое дз(посчитать сколько каждой буквы в строке).pas
+++ 1_course/2_semestr/новое дз(посчитать сколько каждой буквы в строке).pas
@@ -1,14 +1,33 @@
- program sort_letters;
-var s:string;
-    i:integer;
+//Irina. Homework 05.03.18.
+program sort_letters;
+var s: string;
+    j,i,n: integer;
+    p: char;
 begin
-    
- writeln('Fill the string :');
+ cls;
+ write('Fill the string:');
  readln(s);
- for i:=1 to length(s) do
- BEGIN
- write(' ',s[i]);
- end;
+ // Sorting string for counting letters
+ for j:=1 to length(s)-1 do
+  for i:=1 to length(s)-j do
+    if (s[i])>(s[i+1]) then
+     begin
+       p:=s[i];
+       s[i]:=s[i+1];
+       s[i+1]:=p;
+     end;
+ writeln('  Sorted string:',s);
+ //Counting letters in the string
+ p:=s[1];
+ n:=0;
+ for i:= 1 to length(s) do
+   begin
+     if p=s[i] then n:=n+1
+     else begin
+        writeln(p,' - ',n,' letters');
+        p:=s[i];
+        n:=1;
+     end;
+     if (i=length(s)) then writeln(p,' - ',n,' letters');
+   end;
 end.
-  
-    
