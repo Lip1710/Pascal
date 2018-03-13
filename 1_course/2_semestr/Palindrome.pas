93c08467cb7295650abd68a6ad0d75449f20d93e
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-03-13 21:18:05 +0300

    improve Palindrome (HW 12.03.2018)

--- 1_course/2_semestr/Palindrome.pas
+++ 1_course/2_semestr/Palindrome.pas
@@ -1,6 +1,6 @@
 //Irina. HomeWork from 12.03.2018. Palindrome
 program  Palindrome;
-var  s,snew,rev:string;
+var  s,snew,inv:string;
      i: integer;
 begin
  cls;
@@ -8,8 +8,8 @@ begin
  readln(s);
  s:=LowerCase(s);
  snew:='';
- rev:='';
- for i:=1 to length(s) do if s[i]<>' ' then snew:=snew+s[i];  //Remove spaces
- for i:=length(snew) downto 1 do rev:=rev+snew[i];  // Revert
- if (snew=rev) then writeln('It is Palindrome') else writeln('It is not Palindrome');
+ inv:='';
+ for i:=1 to length(s) do if not (s[i] in [' ',',','.',';','`','''','’','"','!']) then snew:=snew+s[i];  //Remove spaces, commas, etc.
+ for i:=length(snew) downto 1 do inv:=inv+snew[i];  // Invert
+ if (snew=inv) then writeln('It is Palindrome') else writeln('It is not Palindrome');
 end.
