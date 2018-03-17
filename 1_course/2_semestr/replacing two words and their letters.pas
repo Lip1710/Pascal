bf5a82cc3d4a386921bbab1a961348e7d533e519
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-03-17 18:54:35 +0300

    the homework of replacing

---
+++ 1_course/2_semestr/replacing two words and their letters.pas
@@ -0,0 +1,26 @@
+//Irina .replacing two words in a string and their letters
+program replace;
+var s,s1,s2,k,p:string;
+    i,n: integer;
+
+begin
+ write('   Fill the string with two words : ');
+ readln(s);
+ //Replacing two words
+ for i:=1 to length(s) do if s[i]=' ' then n:= i;
+ p:='';
+ for i:=1 to (n-1) do p:=p+s[i];
+ k:='';
+ for i:=n+1 to length(s) do k:=k+s[i];
+ s1:=k+' '+p;
+ writeln('Replacing two words: ',s1);
+ // Replacing letters
+ p:='';
+ for i:=1 to (n-1) do p:=s[i]+p;
+ k:='';
+ for i:=n+1 to length(s) do k:=s[i]+k;
+ s1:=k+' '+p;
+ writeln('Way 1 of replacing letters: ',s1);
+ for i:=1 to length(s) do s2:=s[i]+s2;  // Replacing letters 2 (Invert whole string)
+ writeln('Way 2 of replacing letters: ',s2);
+end.
