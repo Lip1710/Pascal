c5a011a67cb8fcf810e4b2b5a3c2d8b1a9538f30
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-03-17 17:41:36 +0300

    Improve "replacing two words..."

--- 1_course/2_semestr/replacing two words in a string.pas
+++ 1_course/2_semestr/replacing two words in a string.pas
@@ -4,26 +4,13 @@ var s,k,p:string;
     i,n: integer;
 
 begin
-writeln('   Fill the string with two words : ');
-readln(s);
- for i :=1 to length(s) do
-  begin
-  if s[i]=' ' then
-    begin
-    //s[i]:=space;
-    n:= i;
-    end;
-   end;
-   p:='';
- for i:=1 to (n-1) do
-  p:=p+s[i];
-  k:='';
- for i:=n+1 to length(s) do
-    begin
-    k:=k+s[i];
-   s:=k+' '+p;
-    end;
+ writeln('   Fill the string with two words : ');
+ readln(s);
+ for i:=1 to length(s) do if s[i]=' ' then n:= i;
+ p:='';
+ for i:=1 to (n-1) do p:=p+s[i];
+ k:='';
+ for i:=n+1 to length(s) do k:=k+s[i];
+ s:=k+' '+p;
  writeln(s);
-
- 
 end.
