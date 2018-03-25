adc4351a66afca722aea151b510272bf3295c563
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-03-25 12:04:41 +0300

    update 19_03

--- 1_course/2_semestr/19_03.pas
+++ 1_course/2_semestr/19_03.pas
@@ -1,29 +1,51 @@
+//Irina. Class Work 19.03.
+// Сколько слов в которых одна или больше гласных
 program glasnye;
-var s1,s:string; v,n,i: integer;
+var s1,s:string; v,n,i,u: integer;
 begin
  v:=0;
- s1:=' ';
+ u:=0;
+ s1:='';
  readln(s);
  for i:=1 to length(s) do
- if s[i]=' ' then n:=i ;
- for i:=0 to (n-1) do
-  begin
-   s1:=s1+s[i];
-   if s[i] in ['A','a','E','e','I','i','O','o','U','u','Y','y','А', 'а','Е', 'е', 'Ё', 'ё', 'И', 'и', 'О', 'о', 'У', 'у', 'Э', 'э', 'Ы', 'ы', 'Ю', 'ю', 'Я', 'я']
-   then
-   v:=v+1;
-
-  end;
- if v>1 then writeln(s1) else
     begin
-     for i:=(n+1) to length(s) do
-  begin
-   s1:=s1+s[i];
-   if s[i] in ['A','a','E','e','I','i','O','o','U','u','Y','y','А', 'а','Е', 'е', 'Ё', 'ё', 'И', 'и', 'О', 'о', 'У', 'у', 'Э', 'э', 'Ы', 'ы', 'Ю', 'ю', 'Я', 'я']
-   then
-   v:=v+1;
-    writeln(s1);
-  end;
+    if s[i]=' ' then
+      begin
+       n:=i;
 
-end;
+
+      end
+    else
+      begin
+
+      end;
+      
+
+    
+    
+    
+    
+    
+
+    for i:=0 to (n-1) do
+       begin
+       s1:=s1+s[i];
+       if s[i] in ['A','a','E','e','I','i','O','o','U','u','Y','y','А', 'а','Е', 'е', 'Ё', 'ё', 'И', 'и', 'О', 'о', 'У', 'у', 'Э', 'э', 'Ы', 'ы', 'Ю', 'ю', 'Я', 'я']
+        then v:=v+1;
+    end;
+ end;
+ if v>=1 then writeln(s1) else
+   begin
+     for i:=(n+1) to length(s) do
+       begin
+       s1:=s1+s[i];
+       if s[i] in ['A','a','E','e','I','i','O','o','U','u','Y','y','А', 'а','Е', 'е', 'Ё', 'ё', 'И', 'и', 'О', 'о', 'У', 'у', 'Э', 'э', 'Ы', 'ы', 'Ю', 'ю', 'Я', 'я']
+       then
+        v:=v+1;
+       writeln(s1);
+       end;
+
+    end;
+writeln('There are ', v,' words with vowels');
+
 end.
