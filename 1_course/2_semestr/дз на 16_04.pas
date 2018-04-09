4d5dfbffa48591bb49d6761996976dfc06100ed4
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-04-09 23:01:12 +0300

    Class work 09.04.2018

---
+++ 1_course/2_semestr/дз на 16_04.pas
@@ -0,0 +1,32 @@
+﻿program tekstoviy;
+var f:text; letters: array['а'..'я'] of integer;s:char;
+    //i:char;
+    n:integer;
+
+begin
+n:=0;
+assign(f, '1.txt');
+ //for i:='а' to 'я' do
+  //letters[i]:=0;
+ 
+ reset(f);
+ 
+ 
+ //read(letters[i]);
+ while not eof(f) do 
+ begin
+  read(f,);
+  read(f,s);
+  if s in ['а'..'я'] then 
+  begin
+  inc(letters[s]);
+  n:=n+1;
+  end;
+ 
+ 
+ close(f);
+
+ 
+ end;
+end. 
+ 
---
+++ 1_course/2_semestr/текстовый файл.pas
@@ -0,0 +1,19 @@
+﻿program tekstoviy;
+var f:text; s:char;
+begin
+ begin
+ assign(f, '1.txt');
+ rewrite(f);
+ writeln(f,'текст');
+ writeln(f,'ещё текст');
+ close(f);
+ end;
+ 
+ reset(f);
+ while not eof(f) do begin
+  read(f,s);
+  writeln(s);
+  end;
+ close(f);
+ 
+end. 
