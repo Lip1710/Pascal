01d7f85dde50deea6b8d00a97fd1a3c5f593835d
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-04-24 23:50:00 +0300

    Class Work 23.04.2018

---
+++ 1_course/2_semestr/graphABC1.pas
@@ -0,0 +1,9 @@
+﻿program graphics;
+uses GrafABC;
+var x,y:integer;
+begin
+ for x:=0 to windowwidth-1 do
+ for y:=0 to windowheight-1 do
+   setpixel(x,y,RGB(2*x-y,x-3*y,x+y));
+end.
+   
---
+++ 1_course/2_semestr/sinusoida.pas
@@ -0,0 +1,35 @@
+﻿program sin;
+uses GraphABC;
+var x,y:integer;
+
+procedure GraphABC.Draw (y:real->real;a:real;b:real;min:real;max:real);
+ begin
+ y:=sin(x);
+ end;
+ 
+//function RGB(a,b,c:byte):Color;
+ //begin
+ //a:=2*x-y;
+ //b:=x-3*y;
+ //c:=x+y;
+
+ //end;
+ //procedure SetPenWidth(Width:integer);
+  //begin
+  //Width:=10;
+  //d:=Width;
+  //end;
+
+begin
+ setWindowSize(600,400);
+ setPenColor(clGreen);
+ //SetPenWidth;
+ //RGB;
+ Line(0,200,600,200);
+ Line(200,0,200,400);
+ GraphABC.Draw;
+
+ 
+ //c:= RGB;
+end. 
+ 
