d49fd1240fa68c32b182f2ebce56291a2b19737a
Lip1710 <35350690+Lip1710@users.noreply.github.com>
2018-04-29 21:43:29 +0300

    from 16.04

--- 1_course/2_semestr/graphABC1.pas
+++ 1_course/2_semestr/graphABC1.pas
@@ -1,9 +1,10 @@
 ﻿program graphics;
-uses GrafABC;
+uses GraphABC;
 var x,y:integer;
 begin
  for x:=0 to windowwidth-1 do
  for y:=0 to windowheight-1 do
-   setpixel(x,y,RGB(2*x-y,x-3*y,x+y));
+     setpixel(x,y,RGB(1*x-y,x-2*y,x*2-y));
+ 
 end.
    
--- 1_course/2_semestr/sinusoida.pas
+++ 1_course/2_semestr/sinusoida.pas
@@ -1,35 +1,41 @@
-﻿program sin;
+﻿program sinus;   //Irina. Paint Sinusoid  graph
 uses GraphABC;
-var x,y:integer;
 
-procedure GraphABC.Draw (y:real->real;a:real;b:real;min:real;max:real);
+procedure PaintGraph;
+ var y,w,h,wm,hm,step,hs:integer;
  begin
- y:=sin(x);
+   ClearWindow;
+   w:=WindowWidth;      // Ширина окна
+   h:=WindowHeight;     // Высота окна
+   wm:=round(w/2);      // Середина окна по ширине
+   hm:=round(h/2);      // Середина окна по высоте
+   step:=round(w/25);   // Шаг синусоиды (частота). Или 20 для неизменяемой частоты
+   hs:=round(h/2)-40;   // Амплитуда синусоиды
+   SetPenWidth(1);
+   setPenColor(clBlack);
+   Line(0,hm,w,hm);     // Ось координат x
+   Line(w-5,hm-5,w,hm); // Ось координат x стрелка
+   Line(w-5,hm+5,w,hm); // Ось координат x стрелка
+   Line(wm,0,wm,h);     // Ось координат y
+   Line(wm,0,wm-5,5);   // Ось координат y стрелка
+   Line(wm,0,wm+5,5);   // Ось координат y стрелка
+   TextOut(w-12,round(h/2+5), 'x'); 
+   TextOut(round(w/2+8),0+5, 'y'); 
+   setPenColor(clBlue);
+   y:=round(hs*sin((0-wm)/step))+hm; 
+   MoveTo(0,y);
+   for var x:=1 to w do begin
+       y:=round(hs*sin((x-wm)/step))+hm; // x-wm - для прохождения графика через центр оси координат (sin(0)=0)
+       LineTo(x,y);
+   end;
+   Redraw;
  end;
  
-//function RGB(a,b,c:byte):Color;
- //begin
- //a:=2*x-y;
- //b:=x-3*y;
- //c:=x+y;
-
- //end;
- //procedure SetPenWidth(Width:integer);
-  //begin
-  //Width:=10;
-  //d:=Width;
-  //end;
-
 begin
+ SetWindowCaption('График синусоиды');
  setWindowSize(600,400);
- setPenColor(clGreen);
- //SetPenWidth;
- //RGB;
- Line(0,200,600,200);
- Line(200,0,200,400);
- GraphABC.Draw;
-
- 
- //c:= RGB;
-end. 
+ LockDrawing;
+ PaintGraph; // Нарисовать график синусоиды
+ OnResize := PaintGraph;
+end.
  
