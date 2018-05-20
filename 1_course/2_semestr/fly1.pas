87780c9db917fb828ae6fb5283762160b61117da
Lip1710 <Lip1710@mail.ru>
2018-05-20 18:58:45 +0300

    new fly

--- 1_course/2_semestr/fly1.pas
+++ 1_course/2_semestr/fly1.pas
@@ -11,25 +11,41 @@ begin
   v0:=10;   // начальная горизонтальаня скорость
   a:=circleabc.Create(0,0,10,clRandom);//(x,y,z,color)
   t:=0; // начальное время
-  while (a.Top+a.Height)<WindowHeight do begin  //падение c начальной горизонтальной скоростью v0 
+  //ymax:=WindowHeight;
+  a.dy:=WindowHeight;a.dx:=0;
+  if t=0 then  a.MoveTo(0,windowHeight);//начальная координата для выброса под углом снизу
+ (* while (a.Top+a.Height)<WindowHeight do begin  //падение c начальной горизонтальной скоростью v0 
      t:=t+0.05; // прирост времени
      tmax:=sqrt((WindowHeight-a.Height)*2/g); // время достижения нижней границы окна
-     x:=v0*t;               // координата x
-     y:=round(sqr(t)*g/2);  // координата y
+     x:=v0*t;               //движение по координате x
+     y:=round(sqr(t)*g/2);  //движение по координате y
      sleep(10);
      if y>(WindowHeight-a.Height) then y:=WindowHeight-a.Height;  //если очередной шаг выходит за границы окна,то останавливаем на границе
      vcur:=g*t; // текущая скорость
      Window.Title := 'x= '+round(x)+'   y= ' + y +Format('  время падения={0:f1}',tmax)+Format(' тек.время={0:f1}',t)+Format(' скорость={0:f0}',round(vcur));
      a.Moveto(round(x),y); // -(Vx*dt,Vy*dt);
-  end;
+  end;*)
   sleep(50);
-  v0:=round(vcur*0.7); //новая начальная скорость после приземления
+  v0:=10;//скорость,если тело выпрыгивает, а не падает
+  while (a.Top +a.Height) > (windowheight/3) do begin 
+   t:=t+0.05;
+   x1:=a.dx; y1:=a.dy;
+   x:=x1+((v0*cos(70*Pi/180)*t+(g*t/2))*d);               //движение по координате x
+   y:=y1-round((v0)*sin(70*Pi/180)*t+(g*sqr(t))/2);//по у
+   sleep(10);
+   vcur:=-g*t;
+   Window.Title := 'x= '+round(x)+'   y= ' + y +Format('  время падения={0:f1}',tmax)+Format(' тек.время={0:f1}',t)+Format(' скорость={0:f0}',round(vcur));
+   a.MoveTo(round(x),y);// - (Vx*dt,Vy*dt);
+   setpixel (a.Center.X,a.Center.Y,clBlack);
+   end;
+  //v0:=round(vcur*0.7); //новая начальная скорость после приземления
   d:=1; //1 - движение направо, -1 - налево
   while True do begin;  // отскоки
      t:=0;
      y:=y-1;
      x:=x+1;
      a.Moveto(round(x),y);
+     setpixel (a.Center.X,a.Center.Y,clBlack);
      x1:=x;
      y1:=y;
      t:=t+0.05;
@@ -51,6 +67,7 @@ begin
         end;
         Window.Title := 'x= '+round(x)+'   y= ' + y +Format('  время падения={0:f1}',tmax)+Format(' тек.время={0:f1}',t)+Format(' скорость={0:f0}',round(vcur));
         a.Moveto(round(x),y);
+        setpixel (a.Center.X,a.Center.Y,clBlack);
         t:=t+0.05;
      end;
      if vcur<7 then begin   //остаточное движение по прямой, если закончились отскоки.
@@ -59,6 +76,7 @@ begin
           sleep(60+i*2);
           if x+i*d > (WindowWidth-a.Width) then break;
           a.Moveto(round(x+i*d),y);
+          setpixel (a.Center.X,a.Center.Y,clBlack);
         end;
         break;
      end;
